312defab5b8bc9f25db2fee180cf5c2c585f6ae9
liyuans <09300690018@alu.fudan.edu.cn>
2016-11-02 15:12:26 +0800

    Update serverspeeder-all.sh

--- serverspeeder-all.sh
+++ serverspeeder-all.sh
@@ -83,7 +83,7 @@ echo "================================================="
 
 
 #下载支持的bin列表
-curl "https://raw.githubusercontent.com/liyuans/serverspeeder/master/serverspeederbin.txt" -o serverspeederbin.txt || { echo "文件下载失败，自动退出，可以前往http://www.91yun.org/serverspeeder91yun手动下载安装包";exit 1; }
+wget "https://raw.githubusercontent.com/liyuans/serverspeeder/master/serverspeederbin.txt" -o serverspeederbin.txt || { echo "文件下载失败，自动退出，可以前往http://www.91yun.org/serverspeeder91yun手动下载安装包";exit 1; }
 
 
 
